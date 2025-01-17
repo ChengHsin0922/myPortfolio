d8f5f8747d8d252dc80e572af415c56ef684c069
Breezy0922 <168520748+ChengHsin0922@users.noreply.github.com>
2025-01-17 11:29:55 +0800

    addShoppingWebsite

--- all.js
+++ all.js
@@ -19,7 +19,6 @@ window.onload = function() {
     let forest2 = document.getElementById('forest2');
     let forest3 = document.getElementById('forest3');
     let bird = document.getElementById('bird');
-    const cursor = document.querySelector('.custom-cursor');
     window.addEventListener('scroll', () => {
         let value = window.scrollY;
         title.style.top = 200+value*0.8+'px';
@@ -27,9 +26,6 @@ window.onload = function() {
         forest2.style.left = value*0.5+'px';
         forest3.style.bottom = -150+value*-0.5+'px';
         bird.style.left = 300-value*0.5+'px';
-    });
-    document.addEventListener('mousemove', (e) => {
-        cursor.style.transform = `translate(${e.clientX-24}px, ${e.clientY-20}px)`;
     });
     document.querySelectorAll('.fillterButton').forEach(button => {
         button.addEventListener('click', function() {
